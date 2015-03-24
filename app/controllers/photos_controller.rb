9e3d61d83c6da1a64ccba758ce66df2eee7212eb
Cordelia Peters <cordelia.peters@gmail.com>
2015-03-23 18:26:26 -0700

    implementing cloudinary

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,7 @@ class PhotosController < ApplicationController
 
   def index
     @photos = Photo.all
+    # @photos.sort! {|a, b| b.created_at <=> a.created_at}
   end
 
   def show
@@ -13,9 +14,18 @@ class PhotosController < ApplicationController
   end
 
   def create
-    @photo = Photo.create()
-    if @photo
+    @photo = Photo.create(url: params[:url], undergrad_id: params[:user_id])
+    # college id and categories
+    if @photo.save
+      render json: @photo
     end
+  end
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+
+    redirect_to :back
   end
 
 end
