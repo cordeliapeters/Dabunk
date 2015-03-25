592fbb1801e8f833efebff3c2e67e173745850fe
Cordelia Peters <cordelia.peters@gmail.com>
2015-03-24 22:08:57 -0700

    Added relationship table and self-join logic for following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+
+# people the user has followed
+  has_many :relationships, :class_name => "Relationship", :foreign_key => "follower_id"
+  has_many :followeds, :through => :relationships
+
+# people the user is following
+  has_many :reverse_relationships, :class_name => "Relationship", :foreign_key => "followed_id"
+  has_many :followers, :through => :reverse_relationships
+
 end
